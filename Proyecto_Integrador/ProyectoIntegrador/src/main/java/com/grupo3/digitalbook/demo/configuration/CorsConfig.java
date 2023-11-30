f19bcdc74b4435eee723612d376d3a3b059bf5a0
pabloc43 <97137873+Pabloc43@users.noreply.github.com>
2023-11-29 21:41:43 -0300

    Update CorsConfig.java

--- Proyecto_Integrador/ProyectoIntegrador/src/main/java/com/grupo3/digitalbook/demo/configuration/CorsConfig.java
+++ Proyecto_Integrador/ProyectoIntegrador/src/main/java/com/grupo3/digitalbook/demo/configuration/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://maquinaria-pro-g3.s3-website-us-east-1.amazonaws.com"); // Cambia esto al dominio de tu aplicación de React
+        config.addAllowedOrigin("*"); // Cambia esto al dominio de tu aplicación de React
         config.setAllowedHeaders(Arrays.asList(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE, HttpHeaders.ACCEPT));
         config.setAllowedMethods(Arrays.asList(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name()));
         config.setMaxAge(3600L);
