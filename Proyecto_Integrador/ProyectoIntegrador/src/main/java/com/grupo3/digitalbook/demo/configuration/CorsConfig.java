2b2adff62051b6110498603abc2532980a743cb9
pabloc43 <97137873+Pabloc43@users.noreply.github.com>
2023-11-29 22:13:15 -0300

    Update CorsConfig.java

--- Proyecto_Integrador/ProyectoIntegrador/src/main/java/com/grupo3/digitalbook/demo/configuration/CorsConfig.java
+++ Proyecto_Integrador/ProyectoIntegrador/src/main/java/com/grupo3/digitalbook/demo/configuration/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://127.0.0.1:5174");
+        config.addAllowedOrigin("http://127.0.0.1:5173");
         config.addAllowedOrigin("http://maquinaria-pro-g3.s3-website-us-east-1.amazonaws.com");// Cambia esto al dominio de tu aplicación de React
         config.setAllowedHeaders(Arrays.asList(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE, HttpHeaders.ACCEPT));
         config.setAllowedMethods(Arrays.asList(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name()));
